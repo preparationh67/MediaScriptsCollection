50a13fcb8ee009fc92d398fce68305c7f50c61dc
PreparationH67 <preparationh67@gmail.com>
2020-03-20 17:17:22 -0500

    Opus to Vorbis
    Switching from opus to vorbis until metadata supported by opus streams in icecast

--- run_ffmpeg_audio_stream.sh
+++ run_ffmpeg_audio_stream.sh
@@ -2,7 +2,7 @@
 set -x
 # ice or udp
 RUN_MODE="ice"
-ENCODE_MODE="opus"
+ENCODE_MODE="vorbis"
 # capture options
 SAMPLE_RATE="48000"
 OUTPUT_BITRATE="256k"
@@ -15,7 +15,7 @@ if [ "$ENCODE_MODE" == "mp3" ]
 then
 	FFMPEG_OPTS+=(-acodec libmp3lame -ab $OUTPUT_BITRATE -content_type audio/mpeg -f mp3)
 else
-	FFMPEG_OPTS+=(-acodec libopus -ab "128k" -compression_level 10 -application audio -content_type audio/ogg -f ogg)
+	FFMPEG_OPTS+=(-acodec libovrbis -ab "256k" -compression_level 10 -application audio -content_type audio/ogg -f ogg)
 fi
 
 # udp options
