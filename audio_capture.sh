92e8d267997aee28033eec50b1b3414df017f372
PreparationH67 <preparationh67@gmail.com>
2024-05-16 15:33:16 -0500

    Update and rename raw_pcm_audio_capture.sh to audio_capture.sh
    Added wav mode

--- audio_capture.sh
+++ audio_capture.sh
@@ -17,6 +17,7 @@ SAMPLE_RATE="96000"
 FORMAT="s32_le"
 # default off
 TIMED=0
+WAV_MODE=0
 while [ -n "$1" ];do
         case "$1" in
                 -t)
@@ -29,7 +30,12 @@ while [ -n "$1" ];do
                         echo "Using default time limit: $TIMELIMIT"
                         ;;
                 -f)
-                        FILENAME="$2.raw"
+                        if [[ $WAV_MODE -eq 0]]
+                        then
+                                FILENAME="$2.raw"
+                        else
+                                FILENAME="$2.wav"
+                        fi
                         shift
                         ;;
                 -a)
@@ -44,16 +50,31 @@ while [ -n "$1" ];do
                         FORMAT="$2"
                         shift
                         ;;
+                -w)
+                        WAV_MODE=1
+                        FILENAME=${FILENAME/.raw/.wav}
+                        ;;
 
         esac
         shift
 done
-
-if [[ $TIMED -eq 1 ]]
+if [[ $WAV_MODE -eq 0 ]]
 then
-        sudo nice -n -15 arecord -D $ALSA_HW -c 2 -r $SAMPLE_RATE -f $FORMAT -t raw -d $TIMELIMIT "$FILENAME"
+        if [[ $TIMED -eq 1 ]]
+        then
+                sudo nice -n -15 arecord -D $ALSA_HW -c 2 -r $SAMPLE_RATE -f $FORMAT -t raw -d $TIMELIMIT "$FILENAME"
+        else
+                sudo nice -n -15 arecord -D $ALSA_HW -c 2 -r $SAMPLE_RATE -f $FORMAT -t raw -d 0 "$FILENAME"
+        fi
 else
-        sudo nice -n -15 arecord -D $ALSA_HW -c 2 -r $SAMPLE_RATE -f $FORMAT -t raw -d 0 "$FILENAME"
+        if [[ $TIMED -eq 1 ]]
+        then
+                sudo nice -n -15 arecord -D $ALSA_HW -c 2 -r $SAMPLE_RATE -f $FORMAT -t wav -d $TIMELIMIT "$FILENAME"
+        else
+                sudo nice -n -15 arecord -D $ALSA_HW -c 2 -r $SAMPLE_RATE -f $FORMAT -t wav -d 0 "$FILENAME"
+        fi
+
 fi
+
 # MP3 Version
 #nice -n 15 arecord -D hw:0,0 -c 2 -r 192000 -f s32_le -t raw -d 0 | lame -r -s 192 --bitwidth 32 --signed --little-endian -V 0 -b 128 -B 320 - | ffmpeg "${ICE_OPTS[@]}"
