6589aad3803d46740eb10762ebeec0d86b9b3048
PreparationH67 <preparationh67@gmail.com>
2024-05-17 20:02:52 -0500

    Update audio_capture.sh
    File owner now original script user

--- audio_capture.sh
+++ audio_capture.sh
@@ -55,8 +55,8 @@ if [[ $DELAY -gt 0 ]]
 then
         sleep $DELAY
 fi
-sudo nice -n -15 arecord -D $ALSA_HW -c 2 -r $SAMPLE_RATE -f $FORMAT -t $MODE -d $TIMELIMIT "$FILENAME"
 
+sudo nice -n -15 su - $USER -c "arecord -D $ALSA_HW -c 2 -r $SAMPLE_RATE -f $FORMAT -t $MODE -d $TIMELIMIT ${FILENAME}"
 
 # MP3 Version
 #nice -n 15 arecord -D hw:0,0 -c 2 -r 192000 -f s32_le -t raw -d 0 | lame -r -s 192 --bitwidth 32 --signed --little-endian -V 0 -b 128 -B 320 - | ffmpeg "${ICE_OPTS[@]}"
